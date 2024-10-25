71b56f0d4a235f1f2ebc72c00ca378a779d878bd
Theodore Carter <111892118+theocarter@users.noreply.github.com>
2024-10-25 17:38:50 -0400

    Added a button for Sys Admin Page
    I want to add a button on the login page for System Administrators to use to login as a system admin. Couldn't finish today, will return later.

---
+++ BidsyHomepage.java
@@ -0,0 +1,12 @@
+package GUI;
+
+import javax.swing.*;
+
+public class BidsyHomepage {
+    private JToolBar pagesBar;
+    private JToolBar.Separator Home;
+    private JToolBar.Separator seprator2;
+    private JToolBar.Separator separator3;
+    private JToolBar.Separator separator4;
+    private JToolBar.Separator separator5;
+}
