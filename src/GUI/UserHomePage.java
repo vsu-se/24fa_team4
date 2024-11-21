cb6207dc6e0c2b8f4386abeb6fae6dca72045c5e
Leslie Dawn <92553647+lestache15@users.noreply.github.com>
2024-11-21 11:51:30 -0500

    created userhomepage gui

---
+++ src/GUI/UserHomePage.java
@@ -0,0 +1,10 @@
+package GUI;
+
+import javax.swing.*;
+
+public class UserHomePage extends JFrame {
+
+    private JTextField searchTextField;
+    private JButton logoutButton;
+    private JTabbedPane tabbedPane1;
+}
