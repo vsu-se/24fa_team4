3ea3878627fe6d58ad5482fca4666fdf37ff41cf
Jasmine <jasmineeguavoen@gmail.com>
2024-12-04 00:00:16 -0500

    Added a User Registration Page
    Just made another panel for the gui.

--- src/GUI/Login.java
+++ src/GUI/Login.java
@@ -17,12 +17,19 @@ public class Login extends JFrame {
     private JTextField textField2;
     private JPasswordField passwordField2;
     private JButton signInButton1;
+    private JButton newUserClickHereButton;
+    private JPanel NewUserRegistrationPanel;
+    private JTextField textField3;
+    private JPasswordField passwordField3;
+    private JButton createAccountButton;
+    private JButton signInButton3;
 
     public Login() {
         // Set up the ParentPanel with CardLayout
         ParentPanel.setLayout(new CardLayout());
         ParentPanel.add(WelcomePanel, "WelcomePanel");
         ParentPanel.add(AdminLogin, "AdminLogin");
+        ParentPanel.add(NewUserRegistrationPanel, "NewUserRegistrationPanel");
 
         // Set up the JFrame
         setContentPane(ParentPanel);
@@ -39,6 +46,15 @@ public class Login extends JFrame {
                 // Switch to AdminLogin panel
                 CardLayout layout = (CardLayout) ParentPanel.getLayout();
                 layout.show(ParentPanel, "AdminLogin");
+            }
+        });
+
+        newUserClickHereButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Switch to Registration Panel for new users.
+                CardLayout layout2 = (CardLayout) ParentPanel.getLayout();
+                layout2.show(ParentPanel, "NewUserRegistrationPanel");
             }
         });
     }
